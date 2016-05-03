02066851ca267f30dd550cf14e48551cab32fb8f
olmesm <olmesm@gmail.com>
2016-05-03 21:21:30 +0100

    added name to crawl

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -2,8 +2,13 @@ class CrawlsController < ApplicationController
 
   def index
     @pub = Crawl.yelp_query(params).businesses.sample
+    @crawls = Crawl.all
   end
-  
+
+
+  def show
+
+  end
   # def new
   #   render json: yelp_query
   # end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503161121) do
+ActiveRecord::Schema.define(version: 20160503202050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160503161121) do
   create_table "crawls", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "pubs", force: :cascade do |t|
