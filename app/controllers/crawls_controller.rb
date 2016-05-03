bc5d529d1f264bc125b1897e76425c4a68f31cdc
olmesm <olmesm@gmail.com>
2016-05-03 15:20:14 +0100

    unit test on yelp api

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -1,2 +1,21 @@
 class CrawlsController < ApplicationController
+
+  def index
+    render :index
+  end
+
+  def new
+    yelp_query
+  end
+
+  private
+
+  def location_params
+    params.permit(:location)[:location]
+  end
+
+  def yelp_query
+    Yelp.client.search(location_params, { term: 'bars and pubs' })
+  end
+
 end
