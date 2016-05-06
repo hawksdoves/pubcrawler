99164ace205332eeb1cbf5a8adaadb78e8d11495
Harsheet <harsheetpatel@hotmail.com>
2016-05-06 21:04:55 +0100

    Removed unnecessary comments and changed app name to Pubcrawler

--- test/e2e/loginFeature.js
+++ test/e2e/loginFeature.js
@@ -1,6 +1,5 @@
 describe('Home page', function() {
   it('has a title', function() {
-    // We don't need to put in the full url as we set baseUrl in our config
     browser.get('/');
     expect(browser.getTitle()).toEqual('Playlists');
   });
