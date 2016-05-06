2e6392c450497ae7e565294ace71c1bc65632aa0
Harsheet <harsheetpatel@hotmail.com>
2016-05-06 21:03:26 +0100

    Removed unnecessary comments and changed app name to Pubcrawler

--- test/unit/controller/ControllerSpec.js
+++ test/unit/controller/ControllerSpec.js
@@ -1,17 +1,13 @@
 describe('PlaylistsCtrl', function() {
-// Load the module with ContainerContactCtrl, add your module name in here!
-  beforeEach(module('starter'));
+  beforeEach(module('Pubcrawler'));
 
     var ctrl;
 
-  // inject the $controller and $rootScope services
-  // in the beforeEach block
   beforeEach(inject(function($controller) {
     ctrl = $controller('PlaylistsCtrl');
   }));
 
   it('should do this and that', function() {
-    // console.log(self.playlists)
     expect(self.playlists[0].title).toEqual("Reggae");
   });
 });
