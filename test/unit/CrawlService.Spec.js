349cbcce957efa405f3805ee647969a71654768b
olmesm <olmesm@gmail.com>
2016-05-08 12:59:02 +0100

    added unit test

---
+++ test/unit/CrawlService.Spec.js
@@ -0,0 +1,24 @@
+describe('CrawlService', function(){
+  beforeEach(module('pubcrawlerApp'));
+
+  var CrawlService, httpBackend;
+
+  var crawlData = { name: 'NameOfNewCrawl', start_postcode: 'postcode' };
+
+  beforeEach(inject(function(_CrawlService_, $httpBackend) {
+    CrawlService = _CrawlService_;
+    httpBackend = $httpBackend;
+    httpBackend
+      .whenPOST('http://localhost:3000', crawlData).respond({
+        name: 'NameOfNewCrawl'
+      });
+  }));
+
+  it('should post crawl to api', function(){
+      CrawlService.addCrawl(crawlData)
+        .then(function(response) {
+          expect(response.data).toEqual({ name: 'NameOfNewCrawl' });
+        });
+      httpBackend.flush();
+  });
+});
