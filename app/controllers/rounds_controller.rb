89f0f5e5fd5bc84fdf0184a1789edd3919901337
olmesm <olmesm@gmail.com>
2016-05-09 12:11:32 +0100

    refac

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,13 +1,10 @@
 class RoundsController < ApplicationController
-# skip_before_filter :verify_authenticity_token, :only => :create
 skip_before_action :verify_authenticity_token
 
 	def update
-		p params
 		Round.log_time params[:id]
 		Round.reveal_next params[:id]
 		round = Round.find params[:id]
-		# redirect_to crawl_path round.crawl
 		render :json => { :status => "success", :message => round.crawl}, :status => 200
   end
 end
