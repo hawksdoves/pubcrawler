7ae317aa5c0263e65c3e1d60c77c9833592775bf
Harsheet <harsheetpatel@hotmail.com>
2016-05-09 17:52:36 +0100

    Checkin button now hides upon clicking checkin button

--- www/js/controllers/RoundsController.js
+++ www/js/controllers/RoundsController.js
@@ -1,43 +1,24 @@
-pubcrawlerApp.controller('RoundsController', ['$state','RoundService', 'CrawlService', function($state, RoundService, CrawlService) {
+pubcrawlerApp.controller('RoundsController', ['$state', '$window', 'RoundService', 'CrawlService', function($state, $window, RoundService, CrawlService) {
 
   var self = this;
 
-  self.roundId = $state.params.id
+  self.roundId = $state.params.id;
 
-  self.updateRound = function(roundId){
-    RoundService.updateRound(roundId);
-    $state.go('app.roundSingle');
+  self.updateRound = function(roundId) {
+    RoundService.updateRound(roundId)
+      .then(function(){
+          $window.location.reload(true);
+      });
   };
-
-  // CrawlService.getSingleCrawl($state.params.crawl_id)
-  //   .then(function(data) {
-  //     self.singleCrawl = data;
-  //   });
 
   CrawlService.getSingleCrawl($state.params.crawl_id)
     .then(roundInfo);
 
-  function roundInfo(data){
-    var myround = data.rounds.filter(findRound)[0];
-    return self.myround = myround;
+  function roundInfo(data) {
+    self.roundDetails  = data.rounds.filter(findRound)[0];
 
     function findRound(round) {
       return round.id == self.roundId;
     }
   }
-
-  // self.isCheckedIn = function() {
-  //   var checkedIn;
-  //   console.log("********");
-  //   self.singleCrawl.rounds.forEach(_pubRoundMatcher);
-  //   return checkedIn;
-  //
-  //   function _pubRoundMatcher(round) {
-  //     if(round.id === self.roundId ) {
-  //       checkedIn = round.checkin;
-  //     }
-  //   }
-  // };
-
-
 }]);
