0a31b212510b95e36a285646b7e3671f8ba5e4f4
olmesm <olmesm@gmail.com>
2016-05-10 13:15:45 +0100

    auth successful,need refac

--- www/js/controllers/AuthController.js
+++ www/js/controllers/AuthController.js
@@ -1,4 +1,40 @@
 pubcrawlerApp.controller('AuthController', ['$state', '$auth', function($state, $auth) {
+  var self  = this;
 
+  self.submitRegistration = function(registrationForm) {
+    $auth.submitRegistration(registrationForm)
+      .then(function(resp) {
+        console.log('then');
+        console.log(resp.headers());
+      })
+      .catch(function(resp) {
+        console.log('catch');
+        console.log(resp);
+      });
+  };
+
+  self.submitLogin = function(loginForm) {
+    $auth.submitLogin(loginForm)
+      .then(function(resp) {
+        console.log('then');
+        // console.log(resp.headers());
+        console.log(resp);
+      })
+      .catch(function(resp) {
+        console.log('catch');
+        console.log(resp);
+      });
+  };
+
+
+  self.signOut = function() {
+      $auth.signOut()
+        .then(function(resp) {
+          console.log('then');
+        })
+        .catch(function(resp) {
+          console.log('catch');
+        });
+    };
 
 }]);
