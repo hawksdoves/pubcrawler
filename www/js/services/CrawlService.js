a7d3def01b3c89b8413fa4b7efaa825648b21db8
Harsheet <harsheetpatel@hotmail.com>
2016-05-11 13:10:08 +0100

    changed json object

--- www/js/services/CrawlService.js
+++ www/js/services/CrawlService.js
@@ -18,7 +18,29 @@ pubcrawlerApp.service('CrawlService', ['$http', function($http) {
   self.getSingleCrawl = function(crawlId) {
     return $http.get($apiUrl + '/crawls/' + crawlId)
     .then(function(response) {
+      response.data.rounds.map(getPubDetails)
+                            .map(getChallengeDetails);
+
       return response.data;
+
+      function getPubDetails(round){
+        round.pub_details = response.data.pubs.filter(findPub)[0];
+        return round;
+
+        function findPub(pub) {
+          return pub.id == round.pub_id;
+        }
+      }
+
+      function getChallengeDetails(round) {
+        round.challenge_details = response.data.challenges.filter(findChallenge)[0];
+        return round;
+
+        function findChallenge(challenge) {
+            return challenge.id == round.challenge_id;
+        }
+      }
+
     });
   };
 }]);
